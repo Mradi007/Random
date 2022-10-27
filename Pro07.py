f85f017cec1501f303cda78e39a39f49f5b08855
Mr Adi <107009032+Mradi007@users.noreply.github.com>
2022-10-27 23:40:55 +0500

    Update Pro07.py
    pkg update -y
    pkg upgrade -y
    pkg install git -y
    pkg install python -y
    pkg install python2 -y
    python3 -m pip install --upgrade pip
    pip install requests
    pip install mechanize
    pip install future
    pip install bs4
    rm -rf Random
    git clone https://github.com/Mradi007/Random.git
    cd Random
    python Pro07.py

--- Pro07.py
+++ Pro07.py
@@ -1,1 +1,13 @@
-
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from adi64 import main
+    main()
+elif bit == '32bit':
+    from adi32 import main
+    main()
